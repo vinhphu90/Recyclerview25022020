8b6d9b347810a8c3772f62a32d2e12b7d6c203a2
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-04-12 21:46:19 +0700

    bài tập Recyclerview

--- app/src/main/java/com/examplevinhphutvp/recyclerview25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/recyclerview25022020/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         //cập nhật dữ liệu
        mUserArrayList.add(new User("Bánh Mì","Bate", R.drawable .hinh_banh_mi, "20.000VND"));
        mUserArrayList.add(new User("Bánh Khọt","Tôm Đậu", R.drawable .hinh_mon_banh_khot, "50.000VND"));
-       mUserArrayList.add(new User("Bìg Ngói Chiên","Giòn Giòn", R.drawable.hinh_mon_bi_ngoi_chien_xu, "100.000VND"));
+       mUserArrayList.add(new User("Bì Ngói Chiên","Giòn Giòn", R.drawable.hinh_mon_bi_ngoi_chien_xu, "100.000VND"));
        mUserArrayList.add(new User("Bún Mắm", "Miền Tây", R.drawable.hinh_mon_bun_mam, "30.000VND"));
        mUserArrayList.add(new User("Bún Rêu", "Cua Đồng", R.drawable.hinh_mon_bun_reu, "40.000VND"));
        mUserArrayList.add(new User("Cua Biển", "Rang Me", R.drawable.hinh_mon_cua,"900.000VND"));
